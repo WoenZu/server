f3d4ed0f76a57ef41066a76a4d4a0563a009876f
Woenzu <0x81zu@gmail.com>
2014-09-16 21:58:17 +0400

    Added check for userdb.json and config.json

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ var fs = require( 'fs' );
 var client = require( 'client' );
 
 var clients = [];
+var appDir = process.env.PWD;
 
-dbCheck();
+initialize();
 
 var server = net.createServer( function( sock ) {
   createClient( sock );
@@ -30,18 +31,34 @@ function createClient( sock ) {
   sock.write( 'MOTD Placeholder... connected to server...\n', 'utf8' );
 }
 
-function dbCheck() {
-  console.log( 'trying to find usersdb.json' );
-  console.log( 'checking in: ' + process.env.PWD + '/users' );
-  var file = process.env.PWD + '/users/usersdb.json';
+function initialize() {
 
+  //TODO refactor duplicate code!!!
+  //server config check
+  var serverConfigPath = appDir + '/config.json';
+  var isExist = checkForFileExistence( serverConfigPath );
+
+  if(!isExist) {
+    fs.writeFileSync( serverConfigPath, '{"configuration":{}}\n' );
+  }
+
+  //user database check
+  var userdbPath = appDir + '/users/userdb.json';
+  isExist = checkForFileExistence( userdbPath );
+
+  if(!isExist) {
+    fs.writeFileSync( userdbPath, '{"users":[]}\n' );
+  }
+}
+
+function checkForFileExistence( filename ) {
   try {
-    fs.openSync( file, 'r+');
-    console.log('file opened successfully');
+    fs.readFileSync( filename );
+    //console.log('[DEBUG]file exists...');
+    return true;
   } catch( e ) {
-    console.log( e );
-    fs.openSync( file, 'w' );
-    console.log( 'file created successfully' );
+    //console.log( '[DEBUG]file does not exist' );
+    return false;
   }
 }
 
