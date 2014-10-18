8a3ae06069eb16e279b3af405f4014654e784600
Woenzu <0x81zu@gmail.com>
2014-10-18 17:48:21 +0400

    Update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 var colors = require('colors');
 var net = require( 'net' );
 var fs = require( 'fs' );
+
+//TODO may be make all tBox modules in one require? var tbox = require( 'tbox' );
+//TODO encoder = new tbox.Encoder( 'key' );
+
 var enc = require( 'tbox/encoder' );
 var mf = require( 'tbox/fabric');
 var client = require( 'tbox/client' );
@@ -19,7 +23,7 @@ var userDB = {};
 initialize();
 
 var server = net.createServer( function( sock ) {
-  sock.write( serverConfig.configuration.MOTD + '\n', 'utf8' );
+  sock.write( serverConfig.configuration.MOTD + '\n', 'utf8' ); //temp
 
   var chatClient = new client.Client( sock );
   clients.push( chatClient );
@@ -30,8 +34,6 @@ var server = net.createServer( function( sock ) {
   sock.on( 'data', function( data ) {
     console.log( '<' + sock.remoteAddress + ':' + sock.remotePort + '> ' + data.white ); // debug
     onReceivingMessage( data );
-
-    sock.write( 'wrong command\n', 'utf8' ); //debug
   });
 
   sock.on( 'error', function( e ) {
@@ -40,7 +42,9 @@ var server = net.createServer( function( sock ) {
 });
 
 function onReceivingMessage( msg ){
-  //TODO process incoming message
+  //TODO unwrap
+  //TODO decode
+  //TODO parse message to message object
 }
 
 function initialize() {
@@ -53,7 +57,7 @@ function initialize() {
 
   //user database check
   var userDBPath = appDir + '/users/userDB.json';
-  var userDBFile = checkForFileExistence( userDBPath, '{"users":[]}\n' );
+  var userDBFile = checkForFileExistence( userDBPath, '{"users":[]}\n' ); //TODO make admin user for default
   userDB = JSON.parse( userDBFile );
   console.log( 'userDB ok.' );
 }
