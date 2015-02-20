1e09b9cf3b614bc48d4fa8430475ce6a2d0b267c
Woenzu <0x81zu@gmail.com>
2015-02-20 18:40:32 +0300

    update

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 'use strict';
 
-var colors = require('colors');
+var colors = require( 'colors' );
 var net = require( 'net' );
 var fs = require( 'fs' );
 var tbox = require( 'tbox' );
 
-var loadFile = tbox.utils.loadFile;
-var createPath = tbox.utils.createPath;
+var loadFile = tbox.tutils.loadFile;
+var createPath = tbox.tutils.createPath;
 var encoder = new tbox.Encoder( 'key' );
 var protocol = new tbox.Protocol();
 var userDB = new tbox.UserDB( createPath( 'userDB.json' ) );
 
-var pool = new tbox.SocketPool();
+var pool = new tbox.ClientPool();
 //var clients = [];
 
 var configPath = createPath( 'config.json' );
 
 //default configuration for server
 var defServerConfig = {};
-defServerConfig.motd = 'Hello Chat World';
-defServerConfig.test = 'test property';
-defServerConfig.port = '6666';
+defServerConfig.Users = [];
+defServerConfig.MOTD = 'Hello Chat World';
+defServerConfig.Test = 'test property';
+defServerConfig.Port = '6666';
 
 var serverConfig = {};
 
@@ -31,13 +32,11 @@ var server = net.createServer( function( sock ) {
   sock.setTimeout( 0 );
 
   var chatClient = new tbox.Client( sock );
+  pool.addClient( chatClient );
 
-  console.log( 'client ' + chatClient.getIP().white + ':' + chatClient.getPort() + ' is connected to server');
+  console.log( 'client ' + chatClient.getIP().white + ':' + chatClient.getPort() + ' is connected to server' );
 
-  pool.push( chatClient );
-  //clients.push( chatClient );//TODO надо ли делать отдельный контейнер или оставить просто массив?
-
-  sock.write( serverConfig.motd + '\n', 'utf8' ); //TODO temp
+  sock.write( serverConfig.MOTD + '\n', 'utf8' ); //TODO temp
 
   sock.on( 'data', function( data ) {
     console.log( '<' + sock.remoteAddress.white + ':' + sock.remotePort + '> ' + data.white ); // debug
@@ -52,47 +51,59 @@ var server = net.createServer( function( sock ) {
     processMessage( msgObj );
   });
 
+  sock.on( 'close', function() {
+    console.log( '<' + chatClient.getIP().white + ':' + chatClient.getPort() + '> is disconnected from server' );
+    //TODO remove this socket from pool
+    pool.removeClient( chatClient );
+  });
+
   sock.on( 'error', function( e ) {
     console.log( e );
   });
 });
 
-function sendTo( id, str ) {
-  // id example {ip: '127.0.0.1:6666', nick: 'woenzu'}
-
-  var client = pool.getClientById( id );
-  try {
-    var sock = client.getSocket();
-    sock.write( str, 'utf8' );
-  } catch ( err ) {
-    console.log( err );
-    console.log('[ERROR] Client is not found.');
-  }
-}
-
 function initialize() {
   console.log( '[DEBUG] Initializing...' );
+
   serverConfig = loadFile( configPath, defServerConfig );//TODO need refactoring (loadFile func and server conf load)
 
   userDB.loadDB();
 }
 
 function processMessage( msg ) {
-  switch ( msg.cmd ) {
+  var command = msg.cmd;
+  var ident = msg.ident;
+  var param = msg.prm;
+
+  switch ( command ) {
     case 'REGISTER':
       console.log( 'Process message REGISTER...' );
-      if ( !userDB.checkDBForUser( msg.id.ip, msg.id.nick ) ) {
+      if ( !userDB.checkForUser( ident, param[ 0 ] ) ) {
         console.log( 'User not available in DB...' );
-        userDB.addUser( userDB.createUser( msg.id.ip, msg.id.nick ) );
-        sendTo( msg.id, protocol.registered( 'Hello, you are registered on server, please wait activation.' ) );
+        userDB.addUser( userDB.createUser( ident, param[ 0 ] ) );
+        sendTo( ident, protocol.registered( 'Hello, you are registered on server, please wait activation.' ) );
       } else {
-        sendTo( msg.id, protocol.registered( 'Welcome to trollbox chat!' ) );
+        sendTo( ident, protocol.registered( 'Welcome to trollbox chat!' ) );
       }
       break;
+    case 'TEXT':
+      //=======
+      break;
     case 'ERROR':
-      console.log( '[ERROR] %s', msg.prm );
+      console.log( '[ERROR] %s', param );
     default:
       //TODO обрабатываем как !неверная комманда протокола!
+  }
+}
+
+function sendTo( ident, str ) {
+  var client = pool.getClientById( ident );
+  try {
+    var sock = client.getSocket();
+    sock.write( str, 'utf8' );
+  } catch ( err ) {
+    console.log( err );
+    console.log( '[ERROR] Client is not found.' );
   }
 }
 
