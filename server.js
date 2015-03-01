e3bb119c11cb205bcfd776ad6c902071db0ac549
Woenzu <0x81zu@gmail.com>
2015-03-01 18:48:53 +0300

    added new messages, sendToAll()...

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var tbox = require('tbox');
 
 var loadFile = tbox.tutils.loadFile;
 var createPath = tbox.tutils.createPath;
+var splitIdent = tbox.tutils.splitIdent;
 var encoder = new tbox.Encoder('key');
 var protocol = new tbox.Protocol();
 var userDB = new tbox.UserDB(createPath('userDB.json'));
@@ -28,15 +29,12 @@ var serverConfig = {};
 initialize();
 
 var server = net.createServer(function(sock) {
-
-  sock.setEncoding('utf8');
-  sock.setTimeout(0);
-
   var chatClient = new tbox.Client(sock);
   pool.addClient(chatClient);
   console.log('client ' + chatClient.getIP().white + ':' + chatClient.getPort() + ' is connected to server');
 
-
+  sock.setEncoding('utf8');
+  sock.setTimeout(0);
   sock.write(protocol.motd(serverConfig.MOTD) + '\n', 'utf8'); //TODO temp
 
   sock.on('data', function(data) {
@@ -62,30 +60,34 @@ var server = net.createServer(function(sock) {
 
 function initialize() {
   console.log('[DEBUG] Initializing...');
-
   serverConfig = loadFile(configPath, defServerConfig);//TODO need refactoring (loadFile func and server conf load)
-
   userDB.loadDB();
 }
 
 function processMessage(msg) {
   var command = msg.cmd;
   var ident = msg.ident;
-  var param = msg.prm;
+  var prm = msg.prm;
+  var spl = splitIdent(ident);
+  var ip = spl[0];
+  var port = spl[1];
 
   switch (command) {
     case 'REGISTER':
-      console.log('Process message REGISTER...');
-      if (!userDB.checkForUser(ident, param[0])) {
+      if (!userDB.checkForUser(ip, prm[0])) {
         console.log('User not available in DB...');
-        userDB.addUser(userDB.createUser(ident, param[0]));
+        userDB.addUser(userDB.createUser(ip, prm[0]));
         sendTo(ident, protocol.registered('Hello, you are registered on server, please wait activation.'));
       } else {
         sendTo(ident, protocol.registered('Welcome to trollbox chat!'));
       }
       break;
     case 'TEXT':
-      //=======
+      if (prm[0] !== '*') {
+        sendTo(prm[0], protocol.text(ident, '', prm[1]));
+      } else {
+        sendToAll(protocol.text('', '', prm[1]));
+      }
       break;
     case 'ERROR':
       console.log('[ERROR] %s', param);
@@ -97,12 +99,22 @@ function processMessage(msg) {
 function sendTo(ident, str) {
   var client = pool.getClientById(ident);
 
+  //console.log('ident: ', ident);
+  //console.log('client: ', client);
+  //console.log(client.getSocket());
   try {
     var sock = client.getSocket();
     sock.write(str, 'utf8');
   } catch (err) {
     console.log(err);
-    console.log('[ERROR] can\'t send message: Client is not found.');
+    console.log('[ERROR] Cannot send message: Client is not found.');
+  }
+}
+
+function sendToAll(str) {
+  for (var i = 0; i < pool.getLength(); i++) {
+    var client = pool.getClientByIndex(i);
+    sendTo(client.getIdent(), str);
   }
 }
 
