009e828ac781c8fd9833b044f843231b028ad6b7
Woenzu <0x81zu@gmail.com>
2016-02-04 22:58:56 +0300

    update

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var serverConfig = {};
 initialize();
 
 var server = net.createServer(function(sock) {
-  var chatClient = {};
+  var chatClient = new tbox.Client(sock);
   sock.write(protocol.motd(serverConfig.MOTD) + '\n', 'utf8'); //TODO temp
   sock.setEncoding('utf8');
   sock.setTimeout(0);
@@ -36,19 +36,17 @@ var server = net.createServer(function(sock) {
 
     console.log('<' + sock.remoteAddress.white + ':' + sock.remotePort + '> ' + data.white); // debug
 
-    var user = {};
+    var userObj = {};
     if(msgObj.cmd === 'REGISTER') { //TODO проверять на целостность и валидность сообщения с данной коммандой
-      chatClient = new tbox.Client(sock);
-
       if(userDB.checkForUser(msgObj.id)) { //если пользователь есть в базе
-        user = userDB.getUser(msgObj.id); // получаем пользователя из базы
-        chatClient.importUserFromDB(user); // импортируем данные пользователя из базы в клиент сервера
+        userObj = userDB.getUser(msgObj.id);// получаем пользователя из базы
+        chatClient.importUserFormDB(userObj); // импортируем данные пользователя из базы в клиент сервера
         pool.addClient(chatClient);
       } else { // если юзера нет в базе
         console.log('User : ' + msgObj.id.white + ' is not available in DB...');
-        user = userDB.createUser(msgObj.id, msgObj.prm[0]);
-        userDB.addUser(user); // добавляем пользователя в базу
-        chatClient.importUserFormDB(user);
+        userObj = userDB.createUser(msgObj.id, msgObj.prm[0]);
+        userDB.addUser(userObj); // добавляем пользователя в базу
+        chatClient.importUserFormDB(userObj);
         pool.addClient(chatClient);
       }
       console.log('client ' + chatClient.getId().white + ' as ' + chatClient.getNick().red + ' is connected to server');
@@ -58,8 +56,12 @@ var server = net.createServer(function(sock) {
   });
 
   sock.on('close', function() {
-    console.log('<' + chatClient.getIP().white + ':' + chatClient.getPort() + '> is disconnected from server');
-    pool.removeClient(chatClient);
+    if(pool.checkForClient(chatClient)) {
+      console.log('<' + chatClient.getIP().white + ':' + chatClient.getPort() + '> is disconnected from server');
+      pool.removeClient(chatClient);
+    } else {
+      console.log('[server] Anonymous connection closed...');// not need to show
+    }
   });
 
   sock.on('error', function(e) {
@@ -68,7 +70,7 @@ var server = net.createServer(function(sock) {
 });
 
 function initialize() {
-  console.log('[DEBUG] Initializing...');
+  console.log('[server] Initializing...');
   serverConfig = loadFile(configPath, defServerConfig);//TODO need refactoring (loadFile func and server conf load)
   userDB.loadDB();
 }
@@ -86,12 +88,9 @@ function processMsgString(msg) {
   var prm = msg.prm;
 
   switch (command) {
-    case 'REGISTER':
+    case 'REGISTER': //TODO провека на состояние клиента pending, active or banned
       var nick = prm[0];
-
       if(!userDB.checkForUser(id)) {
-
-
         sendTo(id, protocol.registered('Hello ' + nick.white + '! You are registered on server, please wait activation. =)'));
       } else {
         sendTo(id, protocol.registered(nick + ' welcome to trollbox chat!'));
@@ -101,36 +100,32 @@ function processMsgString(msg) {
       if(prm[0] !== '*') {
         sendTo(prm[0], protocol.text(id, '', prm[1]));
       } else {
-        sendToAll(protocol.text('', '', prm[1]));
+        sendToAll(protocol.text(id, '*', prm[1]));
       }
       break;
     case 'ERROR':
-      console.log('[ERROR] %s', param);
+      console.log('[server] ERROR %s', param);
     default:
       //TODO обрабатываем как !неверная комманда протокола!
   }
 }
 
-function sendTo(id, str) {
-  var client = pool.getClientById(id);
-  //console.log(client.getSocket());
-
-  console.log('client id: ', client.getId());
-
+function sendTo(userId, str) {
+  var client = pool.getClientById(userId);
   try {
     var sock = client.getSocket();
     sock.write(str, 'utf8');
-    console.log('[DEBUG] string to send: ', str);
+    console.log('[server] string to send: ', str);
   } catch (err) {
     console.log(err);
-    console.log('[ERROR] Cannot send message: Client is not found.');
+    console.log('[server] Cannot send message: Client is not found.');
   }
 }
 
 function sendToAll(str) {
   for (var i = 0; i < pool.getLength(); i++) {
     var client = pool.getClientByIndex(i);
-    sendTo(client.getIdent(), str);
+    sendTo(client.getId(), str);
   }
 }
 
@@ -138,5 +133,5 @@ server.listen({
   host: 'localhost',
   port: 6666
   }, function() {
-    console.log('[DEBUG] Starting server listening at: %s:%s', ip.address(), serverConfig.Port);
+    console.log('[server] Starting server listening at: %s:%s', ip.address(), serverConfig.Port);
 });
